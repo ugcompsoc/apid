d5e0cdfe01033844a2c0342e9a94f6e14210503f
Conor Mc Govern <conor@mcgov.ie>
2023-01-29 21:00:31 +0000

    fix(dockerhub image delete): using different github data to get branch name

--- .github/workflows/delete-branch.sh
+++ .github/workflows/delete-branch.sh
@@ -6,7 +6,7 @@ if [ -z $TAG ]; then
     exit 1
 fi
 
-echo "> Requesting token from "
+echo "> Requesting token from https://hub.docker.com/v2/users/login/"
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKERHUB_USERNAME}'", "password": "'${DOCKERHUB_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | grep -Po '"token":"*\K[^"]*')
 
 echo "> Requesting deletion of $TAG from $REPO -> https://hub.docker.com/v2/repositories/${REPO}/tags/${TAG}/"
