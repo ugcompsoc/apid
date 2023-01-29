fa3e9f781e5d0c005c2923c10708ad8837a70afb
Conor Mc Govern <conor@mcgov.ie>
2023-01-29 19:32:14 +0000

    ci: delete dockerhub tag on branch deletion

---
+++ .github/workflows/delete-branch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Deletes the tag (branch image) passed in as the first argument
+
+if [ -z $TAG ]; then 
+    echo "\e[32mDockerHub tag is not set...\e[0m"
+    exit 1
+fi
+
+curl -i -X DELETE \
+  -H "Accept: application/json" \
+  -H "Authorization: JWT $DOCKERHUB_PASSWORD" \
+  https://hub.docker.com/v2/repositories/$DOCKERHUB_USERNAME/$REPO/tags/$TAG/
