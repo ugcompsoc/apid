9fa01bd7e9c365aba8a70cdcf054294c6b29d4fe
Conor Mc Govern <conor@mcgov.ie>
2023-01-29 20:52:50 +0000

    fix(dockerhub image delete): typo in script path

--- .github/workflows/delete-branch.sh
+++ .github/workflows/delete-branch.sh
@@ -9,8 +9,8 @@ fi
 echo "> Requesting token from "
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKERHUB_USERNAME}'", "password": "'${DOCKERHUB_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | grep -Po '"token":"*\K[^"]*')
 
-echo "> Requesting deletion of $TAG from $REPO -> https://hub.docker.com/v2/repositories/${DOCKERHUB_USERNAME}/${REPO}/tags/${TAG}/"
+echo "> Requesting deletion of $TAG from $REPO -> https://hub.docker.com/v2/repositories/${REPO}/tags/${TAG}/"
 curl -i -X DELETE \
   -H "Accept: application/json" \
   -H "Authorization: Bearer $TOKEN" \
-  "https://hub.docker.com/v2/repositories/${DOCKERHUB_USERNAME}/${REPO}/tags/${TAG}/"
+  "https://hub.docker.com/v2/repositories/${REPO}/tags/${TAG}/"
