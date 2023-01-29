af37e2f586eeb1aeabc853f266b8610ceee23c14
Conor Mc Govern <conor@mcgov.ie>
2023-01-29 22:07:38 +0000

    ci(delete branch fix): delete releases correctly

--- .github/workflows/delete-branch.sh
+++ .github/workflows/delete-branch.sh
@@ -6,6 +6,14 @@ if [ -z $TAG ]; then
     exit 1
 fi
 
+# If the first character is a v and second character is a
+# number then this is likely a release
+echo "${TAG:0:1}"
+echo "${TAG:1:1}"
+if ! [ "${TAG:0:1}" == "v" ] || ! [[ ${TAG:1:1} =~ [0-9] ]]; then
+  TAG=branch-$TAG
+fi
+
 echo "> Requesting token from https://hub.docker.com/v2/users/login/"
 TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKERHUB_USERNAME}'", "password": "'${DOCKERHUB_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | grep -Po '"token":"*\K[^"]*')
 
