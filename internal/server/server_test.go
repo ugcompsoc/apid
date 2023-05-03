6f217d8953ea57c68d9d98d6dd44f77b43f5a60c
Conor Mc Govern <conor@mcgov.ie>
2023-05-03 21:00:42 +0100

    test(server): add unit test

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/ugcompsoc/apid/internal/config"
 )
 
 func TestStop(t *testing.T) {
@@ -49,12 +50,30 @@ func TestStart(t *testing.T) {
 			s.Stop(context.Background())
 		}()
 		err := s.Start(context.Background())
-		assert.NoError(t, err, "expected server to start without without error")
+		assert.NoError(t, err, "expected server to start without error")
+	})
+
+	t.Run("server should not start", func(t *testing.T) {
+		s := &Server{
+			HTTP: &http.Server{
+				Addr: ":8083",
+			},
+		}
+		l, err := net.Listen("tcp", ":8083")
+		assert.NoError(t, err, "expected to be able to listen on port")
+		defer l.Close()
+		err = s.Start(context.Background())
+		assert.Error(t, err, "expected server to start without error")
 	})
 }
 
+// TODO lets make this better
 func TestNewServer(t *testing.T) {
-	t.Run("///TODO", func(t *testing.T) {
-
+	t.Run("happy path", func(t *testing.T) {
+		var config config.Config
+		config.HTTP.ListenAddress = ":8090"
+		config.HTTP.CORS.AllowedOrigins = []string{"*"}
+		s := NewServer(config)
+		assert.IsType(t, &Server{}, s, "expected NewServer to return Server type")
 	})
 }
